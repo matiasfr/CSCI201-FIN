37be5b16e16f7e4d9d5dfe19a2aa08f0601d9005
matias <matiasfr@usc.edu>
2014-11-24 16:02:59 -0800

    updated lobby thread, tweaked gameserver

--- src/Models/PlayerModel.java
+++ src/Models/PlayerModel.java
@@ -18,7 +18,7 @@ public class PlayerModel extends AbstractObjectModel
 	public int playerLocationX;
 	public int playerLocationY;
 	public int playerLocationQuarter;
-	boolean playerTeam;
+	int playerTeam;
 	public ArrayList<ItemModel> playerInventory;
 	public ArrayList<ImageIcon> playerSprites; // all possible images for animation
 	public ImageIcon playerImage; // current sprite
@@ -27,11 +27,11 @@ public class PlayerModel extends AbstractObjectModel
 	
 	//lock on this object for the server
 	public Lock lock = new ReentrantLock();
-	public PlayerModel(int playerID, String playerName, boolean playerTeam)
+	public PlayerModel(int playerID, String playerName, int team)
 	{
 		this.playerID = playerID;
 		this.playerName = playerName;
-		this.playerTeam = playerTeam;
+		this.playerTeam = team;
 		init();
 	}
 	public void init()
