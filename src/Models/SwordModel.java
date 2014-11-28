fa7ebb6b2181dd58364a54ff940ec85aa6f796c8
Varun Angajala <varunkms@gmail.com>
2014-11-28 12:45:09 -0800

    Update SwordModel.java

--- src/Models/SwordModel.java
+++ src/Models/SwordModel.java
@@ -8,9 +8,9 @@ public class SwordModel extends ItemModel
 	 * 
 	 */
 	private static final long serialVersionUID = -561261080992404691L;
-	int damage;
-	ImageIcon swordImage; // what it looks like on the map
-	double cooldownTime;
+	public int damage;
+	public ImageIcon swordImage; // what it looks like on the map
+	public double cooldownTime;
 	public SwordModel(int damage, double cooldownTime)
 	{
 		this.damage = damage;
