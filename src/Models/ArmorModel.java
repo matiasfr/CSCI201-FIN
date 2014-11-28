46876aaf99e812b11f3bab02d9a75a1764056ec1
Varun Angajala <varunkms@gmail.com>
2014-11-28 12:39:35 -0800

    Update ArmorModel.java

--- src/Models/ArmorModel.java
+++ src/Models/ArmorModel.java
@@ -9,7 +9,7 @@ public class ArmorModel extends ItemModel
 	 */
 	private static final long serialVersionUID = -7664639009794107256L;
 	
-	ImageIcon armorImage;  // what it looks like on the map
+	public ImageIcon armorImage;  // what it looks like on the map
 	public int armorPoints;
 	public ArmorModel(int armorPoints)
 	{
